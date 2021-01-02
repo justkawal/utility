56407092cfcce6682c407eb8e9886c087ffa5fe8
Kawaljeet Singh <49296873+justkawal@users.noreply.github.com>
2021-01-02 19:19:06 +0530

    changes

--- lib/src/extensions/list_num_extension.dart
+++ lib/src/extensions/list_num_extension.dart
@@ -1,24 +1,52 @@
 part of utility;
 
 extension UtilityListNum on List<num> {
-
+  ///
+  /// Sorts the algorithm in `O( nLog(n) )` time complexity.
+  ///
   List<num> heapSort({bool inPlace = true}) {
     if (this == null) {
       return null;
     }
-    var length = this.length;
+    return _heapSort(inPlace ? this : List<num>.from(this));
+  }
+
+  List<num> _heapSort(List<num> list) {
+    var length = list.length;
     for (var i = length.divide(2) - 1; i > -1; i--) {
-      //heapify();
+      heapify(list, length, i);
     }
+    for (var i = length - 1; i > 0; i--) {
+      // ---- start ---- do the switching of the child with the root.
+      var temp = list[i];
+      list[i] = list[0];
+      list[0] = temp;
+      // ----- end -----
+      heapify(list, i, 0);
+    }
+    return list;
   }
 
   void heapify(List<num> list, num length, num i) {
-    var largest = i;
-    var left = i * 2 + 1;
-    var right = i * 2 + 2;
+    var largest = i, left = i * 2 + 1, right = i * 2 + 2;
 
-    if(i < length && list[i] < list[largest]){
+    // check if root is less than left child
+    if (left < length && list[i] < list[left]) {
+      largest = left;
+    }
 
+    // check if root is less than right child
+    if (right < length && list[largest] < list[right]) {
+      largest = right;
+    }
+
+    if (largest != i) {
+      // ---- start ---- do the switching of the child with the root.
+      var temp = list[i];
+      list[i] = list[largest];
+      list[largest] = temp;
+      // ----- end -----
+      heapify(list, length, largest);
     }
   }
 }
